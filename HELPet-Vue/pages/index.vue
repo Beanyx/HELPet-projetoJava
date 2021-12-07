3f3f43852c7d351b793405bdd9d233d78e932a01
Beanyx <89416038+Beanyx@users.noreply.github.com>
2021-12-06 23:19:11 -0300

    Update index.vue

--- HELPet-Vue/pages/index.vue
+++ HELPet-Vue/pages/index.vue
@@ -4,13 +4,12 @@
         <div class="mx-auto" style="max-width: 1000px">
           <h1>Animais em lares temporários</h1>
           <b-table striped responsive hover :items = "animais" :fields="fields">
-
-            <template #cell(raca)="data">
-              <NuxtLink :to="`/animais/${data.value}`">
+            </template #cell(categoria)="data"> 
+            <template #cell(nome)="data">
+              <NuxtLink :to="`/animais/${data.item.id}`">
                 <span>{{data.value}}</span>
               </NuxtLink>
             </template>
-
           </b-table>
       </div>
     </div>
@@ -19,14 +18,14 @@
 <script>
 export default {
     async asyncData({$axios}){
-        let data;
+        let animais;
         try{
-        data = await $axios.$get("/animais");
+        animais = await $axios.$get("/animais");
         } catch(e){ //trata o erro se houver
         console.log(e);
         }
-        console.log(JSON.stringify(data));
-        return {data};
+        console.log(JSON.stringify(animais));
+        return {animais};
       },
         data: function(){
             return {
